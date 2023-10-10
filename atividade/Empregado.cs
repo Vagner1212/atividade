fc22d8c55db1edcf90be8a6dc460f8ad90ed8461
Vagner  Silva <106416474+Vagner1212@users.noreply.github.com>
2023-10-09 22:24:52 -0300

    Update Empregado.cs

--- atividade/Empregado.cs
+++ atividade/Empregado.cs
@@ -1,11 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace atividade
+
 {
+    /// <summary>
+    /// A classe Empregado representa um empregado da  empresa. Aqui guarda todas as informações básicas dele,
+    /// como nome, sobrenome, idade, e até salário. A gente também cuida pra que ninguém coloque um salário muito baixo
+    /// nada menos que 1320. Tem  algumas funcionalidades a mais como ajustar a matrícula e definir a 
+    /// data que o empregado entrou na empresa. Bem direto ao ponto!
+    /// </summary>
+  
     internal class Empregado
     {
         public string primeironome { get; private set; }
@@ -60,7 +68,14 @@ namespace atividade
                 }
             }
         }
-
+        /// <summary>
+        /// Esse é um dos construtores para a classe Empregado. Com ele conseguimos criar um novo empregado
+        /// só com as informações  básicas como: nome, sobrenome, idade e data de nascimento. 
+        /// </summary>
+        /// <param name="primeironome">Primeiro nome do empregado.</param>
+        /// <param name="sobrenome">Sobrenome do empregado.</param>
+        /// <param name="idade">Idade do empregado.</param>
+        /// <param name="dataNascimento">Data de nascimento do empregado.</param>
         internal Empregado(string primeironome, string sobrenome, int idade, DateTime dataNascimento)
         {
             this.primeironome = primeironome;
@@ -69,7 +84,18 @@ namespace atividade
             this.dataNascimento = dataNascimento;
 
         }
-
+        /// <summary>
+        /// Esse aqui é outro construtor da class e 'Empregado'!
+        /// Com as informações básicas, ele também permite definir a matrícula, data de contratação e o salário mensal do empregado.
+        /// A gente consegue criar um empregado com todas as informações de uma vez só.
+        /// </summary>
+        /// <param name="primeironome">Primeiro nome do empregado.</param>
+        /// <param name="sobrenome">Sobrenome do empregado.</param>
+        /// <param name="idade">Idade do empregado.</param>
+        /// <param name="dataNascimento">Data de nascimento do empregado.</param>
+        /// <param name="matricula">Matrícula do empregado.</param>
+        /// <param name="dataContratacao">Data de contratação do empregado.</param>
+        /// <param name="salarioMensal">Salário mensal do empregado.</param>
         internal Empregado(string primeironome, string sobrenome, int idade, DateTime dataNascimento, int matricula, DateTime dataContratacao, double salarioMensal)
         {
             this.primeironome = primeironome;
@@ -80,7 +106,9 @@ namespace atividade
             this.SalarioMensal = salarioMensal;
             this.dataContratacao = dataContratacao;
         }
-
+        /// <summary>
+        /// Aumento de 10% no salário do empregado.
+        /// </summary>
         internal void AumentarSalario()
         {
             SalarioMensal += SalarioMensal * 0.1;
